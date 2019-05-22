635b03958c4ec2020b004237e95367640cdecc3b
hamadfauzi <fauzihamad37@gmail.com>
2019-05-22 22:05:06 +0700

    .

---
+++ app/src/main/java/com/example/tp4d/LoginActivity.java
@@ -0,0 +1,7 @@
+package com.example.tp4d;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+
+}
---
+++ app/src/main/java/com/example/tp4d/PermohonanActivity.java
@@ -0,0 +1,6 @@
+package com.example.tp4d;
+
+import android.app.Activity;
+
+public class PermohonanActivity extends Activity {
+}
